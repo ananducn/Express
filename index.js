57ff38cb622762f2472ce308fde789985a681a68
Ananducn <anandu.cn112@gmail.com>
2025-03-07 13:35:56 +0530

    added .env to gitignore

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import "dotenv/config";
+
 import express from "express";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // app.get("/", (req, res) => {
 //   res.send("Hello anime fans!");
