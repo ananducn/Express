e20fb06d0c986b260623340321204673f49af2a9
Ananducn <anandu.cn112@gmail.com>
2025-03-08 15:05:34 +0530

    Updated a spelling mistake

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post("/leafNinja", (req, res) => {
 });
 
 // to get data on all ninjas
-app.get("/leaf", (req, res) => {
+app.get("/leafNinja", (req, res) => {
   res.status(200).send(ninjaData);
 });
 
